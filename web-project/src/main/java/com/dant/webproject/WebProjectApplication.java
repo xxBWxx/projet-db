0cbedac0b43ab4eff3844ee0b4cc6982b8fe6b22
youness240 <essabriyouness@outlook.fr>
2024-03-11 22:49:12 +0100

    Configuration des serveurs pour repartitions data

--- web-project/src/main/java/com/dant/webproject/WebProjectApplication.java
+++ web-project/src/main/java/com/dant/webproject/WebProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import com.dant.webproject.services.DatabaseService;
 
@@ -18,9 +19,11 @@ public class WebProjectApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(DatabaseService databaseService) {
+	CommandLineRunner commandLineRunner(DatabaseService databaseService, Environment env) {
 		return args -> {
 			List<String> columnNames = new ArrayList<>();
+
+			/*------------------Partie serveur gerer par Youness---------------*/
 
 			columnNames.add("youness");
 			columnNames.add("diakite");
@@ -30,8 +33,16 @@ public class WebProjectApplication {
 
 			databaseService.add("test", "baran", "i love coding!!!");
 
-			//Rajout de la condition pour les repartition des donnée
+			// Récupération du port actuel à partir de l'environnement
+			String serverPort = env.getProperty("local.server.port");
+			String name = env.getProperty("spring.profiles.active");
 
+			//Test pour la repartition des donnée en fonction du serveur
+			//Fonctionne trés bien
+			if("8081".equals(serverPort)) {
+				System.out.println("BON, on est bien sur le port "+serverPort);
+				System.out.println(name);
+			}
 
 		};
 	};
