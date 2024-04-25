305fffd5d0355e10f970c297a5315f0104bd6532
xxBWxx <wbaranacikelw@gmail.com>
2024-04-25 10:32:05 +0200

    Fix select service

--- web-project/src/main/java/com/dant/webproject/services/SelectService.java
+++ web-project/src/main/java/com/dant/webproject/services/SelectService.java
@@ -1,6 +1,5 @@
 package com.dant.webproject.services;
 
-
 import com.dant.webproject.dbcomponents.Column;
 import com.dant.webproject.dbcomponents.Operande;
 import com.dant.webproject.dbcomponents.Type;
@@ -10,14 +9,14 @@ import org.springframework.stereotype.Component;
 import java.util.*;
 
 @Component
-public class SelectService implements ISelectService{
+public class SelectService implements ISelectService {
 
     @Autowired
     private final DatabaseManagementService databaseManagementService;
 
     @Autowired
-    public SelectService(DatabaseManagementService databaseManagementService){
-        this.databaseManagementService=databaseManagementService;
+    public SelectService(DatabaseManagementService databaseManagementService) {
+        this.databaseManagementService = databaseManagementService;
     }
 
     public Map<String, List<Object>> selectAll(String tableName) {
@@ -28,33 +27,30 @@ public class SelectService implements ISelectService{
         return res;
     }
 
-    public Map<String, List<Object>> select_cols(String tableName, List<String> col_names){
+    public Map<String, List<Object>> select_cols(String tableName, List<String> col_names) {
         // Récupérer les données de la table
         Map<String, Column> tableData = databaseManagementService.getDatabase().get(tableName);
         // Vérifier si la table existe dans la base de données
         if (tableData == null) {
             throw new IllegalArgumentException(
-                    "Table non trouvée dans la base de données"
-            );
+                    "Table non trouvée dans la base de données");
         }
 
-        for(String columnName : col_names){
+        for (String columnName : col_names) {
             // Vérifier si la colonne existe dans les données de la table
             if (!tableData.containsKey(columnName)) {
                 throw new IllegalArgumentException(
-                        "La colonne " + columnName + " n'existe pas dans la table"
-                );
+                        "La colonne " + columnName + " n'existe pas dans la table");
             }
         }
 
-        Map<String,List<Object>>results=new LinkedHashMap<>();
+        Map<String, List<Object>> results = new LinkedHashMap<>();
 
-        for(String columnName : col_names){
-            results.put(columnName,tableData.get(columnName).getValues());
+        for (String columnName : col_names) {
+            results.put(columnName, tableData.get(columnName).getValues());
         }
 
         return results;
-
 
     }
 
@@ -66,19 +62,15 @@ public class SelectService implements ISelectService{
 
         Map<String, List<Object>> filteredResult = new LinkedHashMap<>();
 
-
-        List<List<Integer>> listind=new ArrayList<>();
-
+        List<List<Integer>> listind = new ArrayList<>();
 
         for (Operande op : listop) {
-            List<Integer> l=new ArrayList<>();
+            List<Integer> l = new ArrayList<>();
             if (op.eval(tableData)) {
-                l=op.getIndice();
+                l = op.getIndice();
             }
             listind.add(l);
         }
-
-
 
         // Initialisation avec les éléments de la première sous-liste
         Set<Integer> indices = new HashSet<>(listind.get(0));
@@ -87,9 +79,6 @@ public class SelectService implements ISelectService{
         for (List<Integer> liste : listind) {
             indices.retainAll(liste);
         }
-
-
-
 
         for (Map.Entry<String, Column> entry : tableData.entrySet()) {
             String currentColumn = entry.getKey();
@@ -106,14 +95,6 @@ public class SelectService implements ISelectService{
         return filteredResult;
     }
 
-
-
-
-//pour Baran
-
-
-
-
-
+    // pour Baran
 
 }
