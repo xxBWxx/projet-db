c5c449fbc6091d58e650d518a1538aa049746424
xxBWxx <wbaranacikelw@gmail.com>
2024-06-05 00:53:14 +0200

    Allow adding datetime

--- web-project/src/main/java/com/dant/webproject/dbcomponents/Column.java
+++ web-project/src/main/java/com/dant/webproject/dbcomponents/Column.java
@@ -37,10 +37,12 @@ public class Column {
                 } catch (Exception e) {
                     values.add(Integer.MIN_VALUE);
                 }
+
                 break;
 
             case STRING:
                 values.add(value);
+
                 break;
 
             case DOUBLE:
@@ -49,10 +51,16 @@ public class Column {
                 } catch (Exception e) {
                     values.add(Double.NaN);
                 }
+
                 break;
 
             case DATETIME_STRING:
-                values.add(formatTimestamp(value));
+                if (value.contains(":")) {
+                    values.add(value);
+                } else {
+                    values.add(formatTimestamp(value));
+                }
+
                 break;
 
             default:
@@ -74,26 +82,4 @@ public class Column {
 
         return formatter.format(instant.atZone(ZoneId.of("America/New_York")));
     }
-
-    // TODO: write a function to convert human readable date format to timestamp and
-    // call it in insert controller (e.g. 2020-01-01 00:07:56 -> 1577833676000000)
-    // (this function does not have to be in this class, might be somewhere else if
-    // more appropriate)
-    // public class TimestampConverter {
-    // public static void main(String[] args) {
-    // String dateTimeStr = "2020-01-01 00:07:56";
-    // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd
-    // HH:mm:ss");
-
-    // // Parse the date and time
-    // LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, formatter);
-
-    // // Convert to an Instant in UTC
-    // long seconds = dateTime.toEpochSecond(ZoneOffset.UTC);
-    // long microseconds = seconds * 1_000_000;
-
-    // // Print the result
-    // System.out.println("Timestamp in microseconds: " + microseconds);
-    // }
-    // }
-} // Vous pouvez ajouter d'autres méthodes si nécessaire
+}
